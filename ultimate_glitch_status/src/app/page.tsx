87ff7fb310a630282bbcc268ed642171eb2a2441
Bazon Bogdan <152993164+sky11fca@users.noreply.github.com>
2025-04-26 07:37:06 +0300

    refurbished

--- ultimate_glitch_status/src/app/page.tsx
+++ ultimate_glitch_status/src/app/page.tsx
@@ -1,16 +1,12 @@
 import Link from "next/link";
 
 import { LatestPost } from "~/app/_components/post";
-import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
-  const session = await auth();
 
-  if (session?.user) {
-    void api.post.getLatest.prefetch();
-  }
+  void api.post.getLatest.prefetch();
 
   return (
     <HydrateClient>
@@ -47,21 +43,9 @@ export default async function Home() {
             <p className="text-2xl text-white">
               {hello ? hello.greeting : "Loading tRPC query..."}
             </p>
-
-            <div className="flex flex-col items-center justify-center gap-4">
-              <p className="text-center text-2xl text-white">
-                {session && <span>Logged in as {session.user?.name}</span>}
-              </p>
-              <Link
-                href={session ? "/api/auth/signout" : "/api/auth/signin"}
-                className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-              >
-                {session ? "Sign out" : "Sign in"}
-              </Link>
-            </div>
           </div>
 
-          {session?.user && <LatestPost />}
+          <LatestPost />
         </div>
       </main>
     </HydrateClient>
